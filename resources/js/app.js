51cc62b26b97b4931c47ebf5e896a6a536d6d3bf
kujira-f <welkinque@yahoo.co.jp>
2020-05-06 17:57:25 +0900

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import './bootstrap'
+import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+import store from './store' // ★　追加
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+
+
+const createApp = async () => {
+  await store.dispatch('auth/currentUser')
+
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App />'
+  })
+}
+
+createApp()
